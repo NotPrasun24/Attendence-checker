32acbc0f2175e73ceb71006241bb1045d2d5e65d
first <wwe.prasunsedaieng@gmail.com>
2024-10-31 22:51:45 +0545

    first

---
+++ index.js
@@ -0,0 +1,24 @@
+// function attendencebutton(){
+//     const a = document.getElementById("att");
+//     a.value = a.value === "absent" ? "present" : "absent";
+// }
+
+function attendencebutton1(){
+    const a = document.getElementById("att");
+    a.value = a.value === "absent" ? "present" : "absent";
+}
+
+function attendencebutton2(){
+    const a = document.getElementById("att2");
+    a.value = a.value === "absent" ? "present" : "absent";
+}
+
+function attendencebutton3(){
+    const a = document.getElementById("att3");
+    a.value = a.value === "absent" ? "present" : "absent";
+}
+
+function attendencebutton4(){
+    const a = document.getElementById("att4");
+    a.value = a.value === "absent" ? "present" : "absent";
+}
